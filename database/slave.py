ea96620d91bbf091995e354bf0fcaa39248e959f
unknown <imfunky505@ymail.com>
2019-04-16 16:47:05 +0200

    fixed some issues

---
+++ database/slave.py
@@ -0,0 +1,78 @@
+'''
+Paramters to this script:Master ip 
+==========================================
+slave has 3 connections with master:
+1-PUB/SUB connection for updating the slave's database
+2-PUB/SUB connection for implementing the iamAlive 
+3-REQ/REP connection for sending the missed data if the slave went down
+====================================================
+slave has 1 connection with the client:
+1-REQ/REP connection for retrieeving user data
+'''
+import sys  
+import zmq
+import json
+import time
+import threading
+import pymongo
+from aliveSender import *
+sys.path.append('../common')
+from dbmanager import *
+
+updateSlavesTopic="55555"
+handleSlavesTopic="9999"
+iamAliveTopic="12345"
+
+
+serveUserPort=55555 #this port will be used to handle users requests (retrieve, delete,insert)
+updateClientsPort=55556 #this port will be used to update clients with the status of slaves
+updateSlavesPort=55557 #this port will be used to update slaves when new insertion or delete happens
+iamAliveSocketPort=55558
+slaveRecoveryHandlerPort=55559 #this port will be used to update the slave with the missed data
+
+
+class Slave:
+    def __init__ (self):
+        self.mydb = DBManager("usersDatabaseSlave1")
+        self.context=zmq.Context()
+        
+        self.toClientSocket=self.context.socket(zmq.REP)
+        self.toClientSocket.bind(f"tcp://127.0.0.1:{serveUserPort}")
+        
+        self.toMasterSocket=self.context.socket(zmq.SUB)
+        self.toMasterSocket.connect(f"tcp://{sys.argv[1]}:{updateSlavesPort}")
+        self.toMasterSocket.setsockopt_string(zmq.SUBSCRIBE, updateSlavesTopic)
+        #self.toMasterSocket.setsockopt(zmq.RCVTIMEO, 30)
+        
+        self.iamAliveSocket=self.context.socket(zmq.PUB)
+        self.iamAliveSocket.bind(f"tcp://127.0.0.1:{iamAliveTopic}")
+
+        self.recoveryHandlerSocket=self.context.socket(zmq.REP)
+
+
+        '''iamAliveThread=threading.Thread(target=self.sendIamAlive,args=())
+        updateDBThread=threading.Thread(target=self.updateDB,args=())
+        recoveryHandlerThread=threading.Thread(target=self.recoverSlave,args=())
+
+        iamAliveThread.start()
+        updateDBThread.start()
+        recoveryHandlerThread.start()'''
+
+        self.run()
+    def run(self):
+        #this method will run in the main thread it will be responsible for receiving requests from the user
+        while True:
+            message=self.toClientSocket.recv_json()
+            messageDict=json.loads(message)
+            toDbDict={
+                "Username":messageDict["Username"]
+            }    
+            result=self.mydb.retrieveOne(toDbDict)
+            print(result)
+
+
+
+
+s=Slave()
+
+
