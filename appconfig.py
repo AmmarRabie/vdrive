b08e35e31ff1a14d9006ee511467c57711f85a41
AmmarRabie <ammaralsayed55@gmail.com>
2019-04-19 14:22:47 +0200

    add appconfig

--- appconfig.py
+++ appconfig.py
@@ -1,13 +1,19 @@
-
+TOKEN_SECRET = "jndsjfhslaLkGwkfjnlsSDGHOJdasf"
 
 
 N = 3 # number of replicas
 T = 1 # every one second
 P = 5 # number of processes
 
-TRACKER_IP = "192.168.1.8" # the master tracker machine ip (in real world, this is a static ip)
-TRACKER_PORTS = ("8082", "8081", "8080",) # for the tracker machine, there is a lot of ports that can be communicated with
-
+TRACKER_IP = "127.0.0.1" # the master tracker machine ip (in real world, this is a static ip)
+TRACKER_PORTS = ("7000",) # for the tracker machine, there is a lot of ports that client can communicated with
+TRACKER_PORTS_KEEPERS = ("8000",) # ports that data keepers communicate with
 #? assume that every machine have the same ports
-DATA_KEEPER_IPS = ("192.168.1.8",) # append here the ips of all the data keeper nodes(machines)
-DATA_KEEPER_PORTS = ("8080", "8081", "8082",) # for each data keeper machine there is a list of propably available ports
+DATA_KEEPER_IPS = ("127.0.0.1", )
+DATA_KEEPER_PORTS = ("6000",) # for each data keeper machine there is a list of propably available ports
+
+
+TRACKER_ALIVE_PORT = "7500"
+
+DATA_KEEPER_ALIVE_SEND_RATE = 1 # send every one second
+DATA_KEEPER_TIME_OUT = DATA_KEEPER_ALIVE_SEND_RATE + 1 # one second more, depends on the network traffic
