387fefa33c7c46f90c79798e198e4dab0570f12c
AmmarRabie <ammaralsayed55@gmail.com>
2019-04-19 20:45:56 +0200

    add the replication functionality

--- tracker/replicator.py
+++ tracker/replicator.py
@@ -1,21 +1,20 @@
+import sys
+sys.path.append("../")
 import time
+import zmq
 from trackerdbcontroller import TrackerDBController
+from common.zmqHelper import zhelper
+from appconfig import TRACKER_TO_KEEPERS_PORTS as KEEPERS_PORTS
 
+MIN_REPLICATION = 3
 class Replicator:
-
     dbManager = None
-
     maximumFilesOnNode = 0
-
     filesLocations = []
-
     nodesStates = {}
 
-
     def __init__(self, dbname="TrackerDB"):
-
         self.db = TrackerDBController(dbname)
-
         # self.db.insertNode(100,"192.168.1.1")
         # self.db.insertNode(110,"192.168.1.2")
         # self.db.insertNode(120,"192.168.1.3")
@@ -26,75 +25,44 @@ class Replicator:
         # self.db.insertFile("aya","6.mp4",100)
         # self.db.insertFile("mai","6.mp4",100)
 
-# ######################################################################
-
-
-    def replicator_process(self):
-        
-        # filesLocations = self.db.retrAllHaveAttrWithValue(["nodeID","fileName"],[None,None])
-
-        ################################ Gets list of files but assume no 2 users have the same filename
+    def replicator_process(self):       
         listOfFiles = self.db.listAllFilesAllUsers()
-
         listOfNodes = self.db.listAliveNodes()
-        print()
-        
-        # print("filesLocations = ",filesLocations)
-        # print()
+        print("listOfFiles", listOfFiles)
 
-        print("listOfFiles",listOfFiles)
-        print()
-
-        # print("listOfNodes = ",listOfNodes)
-        # print()
-    
         listOfNodes = sorted(listOfNodes, key=lambda k: k["numFiles"]) #, reverse=True) 
-        
-        print("listOfNodes = ",listOfNodes)
-        print()
-
-
-        
+        print("listOfNodes=", listOfNodes)
+    
         for file in listOfFiles:
-            # print (file)
+            print("handling file:", file)
 
             # instances = (sum(value["fileName"] == file["fileName"] for value in filesLocations))
             instances = self.db.getInstancesOfFile(file["userID"],file["fileName"])
 
             src = next(item for item in listOfNodes if item["nodeID"] == file["nodeID"])
-            # src = next((item for item in listOfNodes if item["nodeID"] == "Pam"), None)
+            requiredDestNum = MIN_REPLICATION - instances
+            if (requiredDestNum <= 0):
+                continue
 
-            print("src = ", src)
+            print("src = ", src, f"should be transferred to {requiredDestNum} machines")
 
-            # print(instances)
-            if (instances == 1):
-                x = 1
+            destinations = (item for item in listOfNodes if ( item != src ))
+            availableNodesNum = len(destinations)
+            if (availableNodesNum < requiredDestNum):
+                print(f"there is only {availableNodesNum} nodes, but {requiredDestNum} required")
+            selectedMachines = destinations[:max(availableNodesNum, requiredDestNum)]
 
-                dest1 = next(item for item in listOfNodes if ( item != src ))
-
-                dest2 = next(item for item in listOfNodes if ( item != src and item != dest1 ))
-                
-                print("dest1 = ",dest1," dest2 = ",dest2)
-                ############################ Call function to upload to dest1
-
-                ############################ Call function to upload to dest2
-
-            elif (instances == 2):
-
-                dest1 = next(item for item in listOfNodes if ( item != src))
-
-                print("dest1 = ",dest1)
-
-                ############################ Call function to upload to dest1
-
-
-                
-
+            
+            print("selectedMachines=", selectedMachines)
+            srcSocket = zhelper.newSocket(zmq.REQ, src, KEEPERS_PORTS)
+            srcSocket.send_json({
+                "dests": selectedMachines,
+                "file": file
+            })
+            srcSocket.recv() # ack that replication has been done
 
 if __name__ == '__main__':
     replicatorObject = Replicator("TrackerDB")
-
-
     while(True):
-        # replicatorObject.replicator_process()
+        replicatorObject.replicator_process()
         time.sleep(5)
